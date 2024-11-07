1b86408245f336e727f8e1d77623163a4ae1fba0
Subham Mani <manisubham09@gmail.com>
2024-11-07 13:21:44 +0530

    update

--- med-o-next-home/models/Response.ts
+++ med-o-next-home/models/Response.ts
@@ -2,13 +2,13 @@ import mongoose, { Schema } from "mongoose";
 
 export const responseSchema = new Schema(
   {
-    name: { type: String, trim: true },
-    gender: { type: String, trim: true },
-    contactNumber: { type: String, trim: true },
-    email: { type: String, trim: true },
-    state: { type: String, trim: true },
-    city: { type: String, trim: true },
-    address: { type: String, trim: true },
+    name: { type: String, trim: true, required: true },
+    gender: { type: String, trim: true, required: true },
+    contactNumber: { type: String, trim: true, required: true },
+    email: { type: String, trim: true, required: true },
+    state: { type: String, trim: true, required: true },
+    city: { type: String, trim: true, required: true },
+    address: { type: String, trim: true, required: true },
   },
   { timestamps: true }
 );
