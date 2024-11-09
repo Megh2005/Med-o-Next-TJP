b27479140d013094bf13284ff433f6b757d427f0
Megh2005 <megh.deb.civil27@heritageit.edu.in>
2024-11-10 02:02:26 +0530

    Update team member images in AnimatedTestimonialsDemo component

--- med-o-next-home/app/team/page.tsx
+++ med-o-next-home/app/team/page.tsx
@@ -70,14 +70,14 @@ export default function AnimatedTestimonialsDemo() {
         "Implementation was seamless and the results exceeded our expectations. The platform's flexibility is remarkable.",
       name: "Subham Mani",
       designation: "Web Developer @ TJP",
-      src: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "https://witblogpic.s3.amazonaws.com/1729668715434-1ea6ea27-24a5-40af-9ce7-a3d1cbfada2a.jpeg",
     },
     {
       quote:
         "This solution has significantly improved our team's productivity. The intuitive interface makes complex tasks simple.",
       name: "Sayambar Roy Chowdury",
       designation: "ML Engineer @ TJP",
-      src: "https://images.unsplash.com/photo-1623582854588-d60de57fa33f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "https://avatars.githubusercontent.com/u/161194965?v=4",
     },
     {
       quote:
